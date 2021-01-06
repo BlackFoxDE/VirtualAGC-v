315f926c960480a77208a4a9838f3e2ebd823e4e
glowman434 <glowman434@gmail.com>
2021-01-06 12:02:24 +0000

    DSKY Framework

--- test/trace-exclude-mainloop.sh
+++ test/trace-exclude-mainloop.sh
@@ -1,5 +1,5 @@
 echo Trace test program to exclude mainloop
 sh compile.sh
 cd ..
-make simulate_live | grep -v "Z: 004114\|Z: 004115\|Z: 004116\|Z: 004117"
+make simulate_live | grep -v "Z: 004100\|Z: 004101\|Z: 004102\|Z: 004103"
 cd test
